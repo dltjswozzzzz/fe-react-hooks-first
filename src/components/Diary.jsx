bfdf5d9ac3d4d1996f18e748a79d1eb843f7fd6a
lee <zxsunje775@naver.com>
2023-04-28 16:33:48 +0900

    0426

--- src/components/Diary.jsx
+++ src/components/Diary.jsx
@@ -1,19 +1,50 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 const Diary = () => {
   // 이벤트 핸들러부터 state까지 모두 스스로 작성해보시길 바랍니다!
   // 과연 어떤 state를 생성해주어야하고, 또 어떤 이벤트 핸들러가 필요할까요?
   // 실습으로 다뤘던 두 문제를 참고하면 충분히 해결할 수 있으실거라 생각합니다!
+  const [contents, setContents] = useState("");
+  const [title, setTitle] = useState("");
+
+  const removeAll = () => {
+    setContents("");
+    setTitle("");
+  };
+
+  const submit = () => {
+    window.alert(["제목 : " + title + "\n내용 : " + contents]);
+  };
+
+  const handleTitle = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const handleContents = (event) => {
+    setContents(event.target.value);
+  };
 
   return (
     <div className="diary">
-      <input name="subject" placeholder="제목을 입력해주세요" />
+      <input
+        name="subject"
+        placeholder="제목을 입력해주세요"
+        value={title}
+        onChange={handleTitle}
+      />
       <br />
-      <textarea name="content" placeholder="내용을 입력해주세요" />
+      <textarea
+        name="content"
+        placeholder="내용을 입력해주세요"
+        value={contents}
+        onChange={handleContents}
+      >
+        {contents}
+      </textarea>
       <br />
-      <button>초기화</button>
+      <button onClick={removeAll}>초기화</button>
       <br />
-      <button>입력 완료</button>
+      <button onClick={submit}>입력 완료</button>
     </div>
   );
 };
